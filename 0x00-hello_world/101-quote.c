55616f8a5f775b70ee6807917df2b3a8b6550058
Your Name <you@example.com>
2023-04-23 20:55:39 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <unixstd.h>
+#include <unistd.h>
 /**
- * main - A C program that prints a new line to standard error
- * Return 1 (Success)
+ *main - A C program that prints a new line to standard error
+ *Return 1 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main- A program that print a line with a puts function
- * return : 0 (Success)
+ *main - A program that print a line with a puts function
+ *return : always 0 (Success)
  */
 int main(void)
 {
