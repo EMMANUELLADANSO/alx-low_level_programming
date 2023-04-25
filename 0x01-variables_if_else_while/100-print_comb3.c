cee9cd33e43ed53ef5ca42f9ff89194d737ba357
Your Name <you@example.com>
2023-04-24 22:09:08 -0700

    t

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - print if the number is positive, zero or negative
+ *
+ * Desription: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
+ */
+int main (void)
+{
+int d, p;
+
+for (d = '0'; d < '9'; d++)
+{
+	for(p = '0'; p <= '9'; p++)
+	{
+		if (p != d)
+		{
+			putchar(d);
+			putchar(p);
+			if (d == '8' && p == '9')
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
