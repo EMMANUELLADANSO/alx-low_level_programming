c9757804a64a48d73661a4b85788eac7ba02ae95
Your Name <you@example.com>
2023-05-04 19:24:40 -0700

    more arrays involved

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcat - concatenate two strings
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n);
+{
+
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+#More pointers, more arrays and strings
+A prototype of all functions and the prototype of the functions _putchar should be included in the header file main.h
