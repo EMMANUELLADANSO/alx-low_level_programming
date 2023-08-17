7186225d0791b95242a503e13132d32a30ea83ce
EMMANUELLADANSO <dansonuella@gmail.com>
2023-08-16 18:23:52 -0700

    Update

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
+ * isalpha.c - Make lowercase, uppercase
  * main - Entry point
  * Description: 'prints _putchar to stdout'
  * Return: always 0
  */
 int _isalpha(int c)
 {
-	if ((c >= 97 || c <= 122) || (c >= 65 || c <= 90))
+	if ((c >= 97 || c <= 122) || (c >= 33 || c <= 90))
 	{
 		return (1);
 	}
