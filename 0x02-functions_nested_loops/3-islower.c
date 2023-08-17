78803e5346175d6db5834fec9d7b0ac8827171db
EMMANUELLADANSO <dansonuella@gmail.com>
2023-08-16 18:49:44 -0700

    Update

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _islower - return something if is lowercase
+ * @c: The character must be checked
  * main - Entry point
  * Description: 'prints _putchar to stdout'
  * Return: 1 for lowercase and 0 for anything else
